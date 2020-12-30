c7bf93f8919ac1cdd60287a9676f420da07e24cd
werd2000 <walter.enrique.ruiz.diaz@gmail.com>
2020-12-30 20:21:07 -0300

    Login Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = response ) => {
 
@@ -53,6 +54,52 @@ const login = async ( req, res = response ) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const { name, email, picture } = await googleVerify( googleToken );
+
+        const usuarioDB = await Usuario.findOne({ email});
+        let usuario;
+
+        if (!usuarioDB) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: ';)',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        // Guardar el usuario en BD
+        await usuario.save();
+        
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuario.id )
+
+
+        res.json({
+            ok: true,
+            token
+        });
+        
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token inválido'
+        });
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
